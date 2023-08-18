b7e89901f31f86502c636f1be6263ff71392a2bf
ahmed-ghallap <ahmedor759@gmail.com>
2023-08-18 18:52:57 +0300

    pwa final

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open("static")
+        .then(cache => {
+            return cache.addAll([
+                "./", "./master.js", "./helpers.js", 
+                "./icons/*"
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', e => {
+    e.respondeWith(
+        caches.match(e.request)
+        .then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
